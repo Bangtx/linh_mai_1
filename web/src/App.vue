244e4daa8dd9e3ebf513629745438f87dc752db7
Tran Xuan Bang <bangtx@codenet.jp>
2022-01-20 08:59:43 +0700

    www

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<!--    <router-link to="/">Home</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
   </div>
   <hello-world/>
 </template>
@@ -30,16 +30,16 @@ export default App
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
+/*#nav {*/
+/*  padding: 30px;*/
+/*}*/
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+/*#nav a {*/
+/*  font-weight: bold;*/
+/*  color: #2c3e50;*/
+/*}*/
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+/*#nav a.router-link-exact-active {*/
+/*  color: #42b983;*/
+/*}*/
 </style>
